43d4981bfec56e681ea56ab6b49ce7cdbf0f4f94
SABILUL MUHTADIN <sabilulmuhtadin000@gmail.com>
2026-07-02 15:32:20 +0700

    nomer

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -240,7 +240,7 @@ export default function ContactPage() {
 
       {/* GLOBAL FLOATING WHATSAPP BUTTON (FAB) */}
       <a 
-        href="https://wa.me/6281234567890?text=Halo%20Satya%20%26%20Partners,%20saya%20butuh%20bantuan%20hukum%20segera." 
+        href="https://wa.me/6287788216357?text=Halo%20Satya%20%26%20Partners,%20saya%20butuh%20bantuan%20hukum%20segera." 
         target="_blank" 
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 z-[999] bg-[#25D366] hover:bg-[#1ebd5a] text-white p-4 rounded-full shadow-[0_4px_20px_rgba(37,211,102,0.4)] hover:shadow-[0_4px_25px_rgba(37,211,102,0.6)] hover:-translate-y-1 transition-all duration-300 group flex items-center justify-center animate-bounce-slow"
